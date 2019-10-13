a4198b1a7338adaf26d159fc10dc1e603f06943b
edem <edem.ibraimov.2000@mail.ru>
2019-10-13 16:30:34 +0300

    redrawing dots on R change has been added

--- src/main/java/com/mac/pip4/another_attempt/lab4/Lab4Application.java
+++ src/main/java/com/mac/pip4/another_attempt/lab4/Lab4Application.java
@@ -1,13 +1,23 @@
 package com.mac.pip4.another_attempt.lab4;
 
+import com.mac.pip4.another_attempt.lab4.model.User;
+import com.mac.pip4.another_attempt.lab4.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Lab4Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Lab4Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Lab4Application.class, args);
+
+		//#for tests on localhost
+		/*context.getBean(UserRepository.class).save(
+				new User("admin",
+						context.getBean(BCryptPasswordEncoder.class).
+								encode("admin")));
+		*/
 	}
 
 }
